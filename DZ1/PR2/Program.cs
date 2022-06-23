145911ed412e9c3d3512feb56af92b00182b6014
Denis <9581886@gmail.com>
2022-06-23 23:03:28 +0300

    first commit

---
+++ DZ1/PR2/Program.cs
@@ -0,0 +1,17 @@
+﻿// задача №2
+Console.WriteLine("Задача №2");
+Console.WriteLine("----------------------------------------");
+
+Console.Write("Введите первое целое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе целое число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB) 
+{
+    Console.WriteLine("Первое число (" + numberA + ") больше второго (" + numberB +")");
+}
+else
+{
+    Console.WriteLine("Второе число (" + numberB + ") больше первого (" + numberA +")");
+}
