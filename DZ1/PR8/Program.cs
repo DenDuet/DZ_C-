ab3a8bf7353302292990fa8e9773db3af6a6f8e9
Denis <9581886@gmail.com>
2022-06-23 23:06:42 +0300

    first commit

---
+++ DZ1/PR8/Program.cs
@@ -0,0 +1,17 @@
+﻿// следующая задача №8
+
+Console.WriteLine("Задача №8");
+Console.WriteLine("----------------------------------------");
+
+Console.Write("Введите целое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int i=1;
+
+while (i<=numberA)
+{
+    if (i % 2 == 0) 
+    {
+        Console.Write(i + ", ");
+    }
+    i++;
+}
