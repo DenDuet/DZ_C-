84893742b1f6fe5e67bb4dd92b0c5031743c3057
Denis <9581886@gmail.com>
2022-06-30 22:36:44 +0300

    Задача 19

---
+++ DZ1/PR19/Program.cs
@@ -0,0 +1,22 @@
+﻿// следующая задача №19
+
+Console.WriteLine("Задача №19");
+Console.WriteLine("----------------------------------------");
+
+
+Console.WriteLine("Введите пятизначное число: ");
+string val=Console.ReadLine();
+int v=val.Length;
+Console.WriteLine("Длина числа " + v + " число " + val);
+
+if (v==5)
+{
+    if (val[0]==val[v-1] && val[1]==val[v-2])
+    {
+        Console.WriteLine("Это полиндром");
+    }
+    else
+    {
+        Console.WriteLine("Это не полиндром");
+    }
+}
