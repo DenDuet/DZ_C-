1ea7dec54a0f2cb7b959107b2226dc656752582b
Denis <9581886@gmail.com>
2022-06-30 22:11:31 +0300

    Добавил шапку

---
+++ DZ1/PR23/Program.cs
@@ -0,0 +1,32 @@
+﻿// следующая задача №23
+
+Console.WriteLine("Задача №23");
+Console.WriteLine("----------------------------------------");
+
+int[] cube(int n)
+{
+    int[] array= new int[n];
+
+    for (int i=1; i<=n; i++)
+    {
+        array[i-1] = i * i * i;
+        Console.WriteLine("Куб числа " + i + " равен числу " + Math.Pow(i,3));
+    }
+    return array;
+}
+void printArray(int[] array, int n)
+{
+    Console.Write("Массив ");
+    for (int i=1; i<=n; i++)
+    {
+        Console.Write(array[i-1] + ", ");
+    }
+
+}
+
+Console.Write("Введите число N: ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+
+int[] array = cube(N);
+printArray(array, N); 
