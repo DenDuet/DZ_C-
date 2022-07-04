84a1410021fcaab5fb45f35994555b95ed1d82c4
Denis <9581886@gmail.com>
2022-07-04 21:42:51 +0300

    Задача 25

---
+++ DZ1/PR25/Program.cs
@@ -0,0 +1,11 @@
+﻿// следующая задача №25
+
+Console.WriteLine("Задача №25");
+Console.WriteLine("----------------------------------------");
+
+Console.Write("Введите первое число a= ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число b= ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(a + " в степени " + b + " равно " + Math.Pow(a, b));
