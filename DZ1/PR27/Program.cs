18c30e152c9a6b6fe8de156371f0057b404cce67
Denis <9581886@gmail.com>
2022-07-04 22:09:37 +0300

    Задача 27

---
+++ DZ1/PR27/Program.cs
@@ -0,0 +1,18 @@
+﻿// следующая задача №27
+
+Console.WriteLine("Задача №27");
+Console.WriteLine("----------------------------------------");
+
+int sum(string num)
+{
+    int summa=0;
+    int len=num.Length;
+    for (int i=0; i<len; i++)
+    {
+         summa=summa+int.Parse(num[i].ToString());
+    }
+    return summa;
+}
+Console.Write("Введите число: ");
+string a = Console.ReadLine();
+Console.WriteLine("Сумма чисел этого числа = " + sum(a));
