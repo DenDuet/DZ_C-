7a0e8c3058d32627a49f6cd840f9b4b469e5cd5c
Denis <9581886@gmail.com>
2022-07-17 22:08:07 +0300

    Задача 47

---
+++ DZ1/PR47/Program.cs
@@ -0,0 +1,21 @@
+﻿// следующая задача №47
+
+Console.WriteLine("Задача №47");
+Console.WriteLine("----------------------------------------");
+
+Console.WriteLine("Введите число строк массива: ");
+int m=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов массива: ");
+int n=Convert.ToInt32(Console.ReadLine());
+double[,] array = new double[m,n];
+
+Random rnd = new Random();
+for (int i=0; i<m; i++)
+{
+    for (int j=0; j<n; j++)
+    {
+        array[i,j] = rnd.NextDouble();
+        Console.Write(array[i,j]+" ");
+    }
+    Console.WriteLine();
+}
