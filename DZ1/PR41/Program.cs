78d41459f560e40edcdf1d6973729dc44047bc53
Denis <9581886@gmail.com>
2022-07-17 19:11:11 +0300

    Задача 41

---
+++ DZ1/PR41/Program.cs
@@ -0,0 +1,28 @@
+﻿// следующая задача №41
+
+Console.WriteLine("Задача №41");
+Console.WriteLine("----------------------------------------");
+
+int FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    int numbers = 0;
+    while (index < length)
+    {
+        Console.Write("Введите число: ");
+        collection[index] = Convert.ToInt32(Console.ReadLine());
+        if (collection[index] > 0)
+        {
+            numbers++;
+        }
+        index++;
+    }
+    return numbers;
+}
+
+Console.WriteLine("Сколько чисел в массиве? ");
+int digit = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[digit];
+int numbers = FillArray(array);
+Console.WriteLine("Введено " + numbers + " положительных чисел.");
